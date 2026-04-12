0624e22f546581f6ff2b6432985c106e1c917807
Sneha Jadhav <135454332+sj1304@users.noreply.github.com>
2026-04-12 21:48:10 +0530

    Designer part of home

---
+++ HomeForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace SQLQuest        // ← must match
+{
+    partial class HomeForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblPlayerName = new System.Windows.Forms.Label();
+            this.panelStats = new System.Windows.Forms.Panel();
+            this.lblStatsTitle = new System.Windows.Forms.Label();
+            this.dgvLevels = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelStats.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLevels)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSubtitle
+            // 
+            this.lblSubtitle.Font = new System.Drawing.Font("Courier New", 12F);
+            this.lblSubtitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(160)))), ((int)(((byte)(220)))));
+            this.lblSubtitle.Location = new System.Drawing.Point(149, 206);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(620, 25);
+            this.lblSubtitle.TabIndex = 1;
+            this.lblSubtitle.Text = "Learn SQL by escaping the House!";
+            this.lblSubtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.txtPlayerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPlayerName.Font = new System.Drawing.Font("Courier New", 11F);
+            this.txtPlayerName.ForeColor = System.Drawing.Color.White;
+            this.txtPlayerName.Location = new System.Drawing.Point(786, 261);
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(200, 28);
+            this.txtPlayerName.TabIndex = 3;
+            this.txtPlayerName.Text = "Shravni";
+            this.txtPlayerName.TextChanged += new System.EventHandler(this.txtPlayerName_TextChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(60)))), ((int)(((byte)(180)))));
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Courier New", 13F, System.Drawing.FontStyle.Bold);
+            this.btnPlay.ForeColor = System.Drawing.Color.Black;
+            this.btnPlay.Location = new System.Drawing.Point(606, 397);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(368, 56);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblPlayerName
+            // 
+            this.lblPlayerName.Font = new System.Drawing.Font("Courier New", 11F);
+            this.lblPlayerName.ForeColor = System.Drawing.Color.White;
+            this.lblPlayerName.Location = new System.Drawing.Point(591, 263);
+            this.lblPlayerName.Name = "lblPlayerName";
+            this.lblPlayerName.Size = new System.Drawing.Size(189, 25);
+            this.lblPlayerName.TabIndex = 2;
+            this.lblPlayerName.Text = "Enter Your Name:";
+            this.lblPlayerName.Click += new System.EventHandler(this.lblPlayerName_Click);
+            // 
+            // panelStats
+            // 
+            this.panelStats.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(30)))), ((int)(((byte)(70)))));
+            this.panelStats.Controls.Add(this.lblStatsTitle);
+            this.panelStats.Controls.Add(this.dgvLevels);
+            this.panelStats.Location = new System.Drawing.Point(12, 474);
+            this.panelStats.Name = "panelStats";
+            this.panelStats.Size = new System.Drawing.Size(1497, 321);
+            this.panelStats.TabIndex = 5;
+            // 
+            // lblStatsTitle
+            // 
+            this.lblStatsTitle.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatsTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(215)))), ((int)(((byte)(0)))));
+            this.lblStatsTitle.Location = new System.Drawing.Point(51, 14);
+            this.lblStatsTitle.Name = "lblStatsTitle";
+            this.lblStatsTitle.Size = new System.Drawing.Size(827, 30);
+            this.lblStatsTitle.TabIndex = 0;
+            this.lblStatsTitle.Text = "LEADERBOARD AND LEVEL STATUS";
+            // 
+            // dgvLevels
+            // 
+            this.dgvLevels.AllowUserToAddRows = false;
+            this.dgvLevels.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLevels.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(50)))), ((int)(((byte)(150)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(215)))), ((int)(((byte)(0)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvLevels.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvLevels.ColumnHeadersHeight = 29;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(30)))), ((int)(((byte)(70)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Courier New", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvLevels.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvLevels.EnableHeadersVisualStyles = false;
+            this.dgvLevels.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(60)))), ((int)(((byte)(120)))));
+            this.dgvLevels.Location = new System.Drawing.Point(57, 47);
+            this.dgvLevels.Name = "dgvLevels";
+            this.dgvLevels.ReadOnly = true;
+            this.dgvLevels.RowHeadersVisible = false;
+            this.dgvLevels.RowHeadersWidth = 51;
+            this.dgvLevels.Size = new System.Drawing.Size(1368, 262);
+            this.dgvLevels.TabIndex = 3;
+            this.dgvLevels.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLevels_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.lblSubtitle);
+            this.panel1.Location = new System.Drawing.Point(294, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(946, 251);
+            this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Courier New", 13F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(562, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Level 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Courier New", 13F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(725, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 45);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Level 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Courier New", 13F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(889, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 45);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Level 3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // HomeForm
+            // 
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(1519, 796);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblPlayerName);
+            this.Controls.Add(this.txtPlayerName);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.panelStats);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "HomeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Quest";
+            this.Load += new System.EventHandler(this.HomeForm_Load);
+            this.panelStats.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLevels)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblSubtitle, lblPlayerName;
+        private System.Windows.Forms.Label lblStatsTitle;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Panel panelStats;
+        private System.Windows.Forms.DataGridView dgvLevels;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
